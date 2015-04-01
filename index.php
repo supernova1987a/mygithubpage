127c6ccccfd52a991e86c20e58bb87f2de920fd1
supernova1987a <pradiptashrestha@gmail.com>
2015-04-01 00:01:43 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+
+Will PHP work?
+
+*/
+
+?>
+
+<?php 
+echo '<html><title>Pradipta.me</title><body><p>PHP, MySQL, Java, C, Python, T-SQL, SQL, HTML5, CSS, JavaScript</p></body></html>'; 
+?>
